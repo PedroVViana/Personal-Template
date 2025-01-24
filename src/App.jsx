54df7253c56bf448722606d0155cef3c98776ffd
PedroVViana <pedro.vviana@hotmail.com>
2025-01-24 18:23:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>
+        Site Personal
+      </h2>
+    </>
+  )
+}
+
+export default App
